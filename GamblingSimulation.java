e74ea08aa831073dd4cca3ffc820a3e9b3796135
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-10-19 01:59:32 +0530

    Added to know the days won and lost and by how much

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -5,6 +5,10 @@ public class GamblingSimulation {
 	static final int bet = 1;
 	static int gamblerTotalStake = 100;
 	static final int won = 1;
+	static int totalDaysWon;
+	static int totalDaysLost;
+	static int totalAmountWonInMonth;
+	static int totalAmountLostInMonth;
 
 	public static void main(String[] args) {
 		for (int day = 1; day <= 30; day++) {
@@ -16,6 +20,8 @@ public class GamblingSimulation {
 				bet(day);
 			}
 		}
+		System.out.println("\nTotal Days won in a month: " + totalDaysWon + ", By amount: " + totalAmountWonInMonth);
+		System.out.println("Total Days lost in a month: " + totalDaysLost + ", By amount: " + totalAmountLostInMonth);
 	}
 
 	public static void bet(int day) {
@@ -40,8 +46,14 @@ public class GamblingSimulation {
 
 		if (amountBeforeBet < gamblerTotalStake) {
 			System.out.println("On Day-" + day + ", Gambler won total amount: " + amountWon);
+			totalDaysWon++;
+			totalAmountWonInMonth += amountWon;
 		} else if (amountBeforeBet > gamblerTotalStake) {
 			System.out.println("On Day-" + day + ", Gambler lost total amount: " + amountLost);
+			totalDaysLost++;
+			totalAmountLostInMonth += amountLost;
 		}
+
+		System.out.println("Gambler remaining amount: " + gamblerTotalStake);
 	}
 }
