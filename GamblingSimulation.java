e37240f53a448e5bc07cbc4a21eaa5add0417e7c
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-10-19 01:48:29 +0530

    Added to know the total amount won or lost of every day

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -7,23 +7,41 @@ public class GamblingSimulation {
 	static final int won = 1;
 
 	public static void main(String[] args) {
-		bet();
+		for (int day = 1; day <= 30; day++) {
+			if (gamblerTotalStake <= 0) {
+				System.out.println("\nGambler has no amount to Bet, amount: " + gamblerTotalStake);
+				break;
+			} else {
+				System.out.println("\nDay-" + day + " started with amount: " + gamblerTotalStake);
+				bet(day);
+			}
+		}
 	}
 
-	public static void bet() {
-		
+	public static void bet(int day) {
+
 		int stake = gamblerTotalStake / 2;
 		int lostStake = gamblerTotalStake - stake;
 		int wonStake = gamblerTotalStake + stake;
-		
+		int amountBeforeBet = gamblerTotalStake;
+
 		while ((gamblerTotalStake < wonStake && gamblerTotalStake > lostStake) || gamblerTotalStake == 1) {
 			int toss = new Random().nextInt(2);
-			
+
 			if (toss == won) {
 				gamblerTotalStake += 1;
 			} else {
 				gamblerTotalStake -= 1;
 			}
+		}
+
+		int amountWon = gamblerTotalStake - amountBeforeBet;
+		int amountLost = amountBeforeBet - gamblerTotalStake;
+
+		if (amountBeforeBet < gamblerTotalStake) {
+			System.out.println("On Day-" + day + ", Gambler won total amount: " + amountWon);
+		} else if (amountBeforeBet > gamblerTotalStake) {
+			System.out.println("On Day-" + day + ", Gambler lost total amount: " + amountLost);
 		}
 	}
 }
